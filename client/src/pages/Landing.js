3c86105ff9cc106afb829131230d14f53239deac
akash-9606 <akashkolaki5@gmail.com>
2025-05-06 15:14:07 +0530

    Add files via upload

---
+++ client/src/pages/Landing.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import Navbar from '../components/navbar'
+import FooterSection from '../components/footer'
+import bannerPage from "../images/Landing.png"
+import AmbulanceScene from '../components/AmbulanceAnimation'
+import Testimonial from '../components/Testimonials'
+import Hero from '../components/hero'
+import App from '../components/animation'
+import "../styles/landing.css"; 
+
+const LandingPage = () => {
+  return (
+    <>
+      <Navbar />
+
+      <div className="landing-wrapper">
+        <div className="banner-container">
+          <img src={bannerPage} alt="Landing Banner" className="banner-image" />
+        </div>
+      </div>
+
+
+      <Hero />
+      <App/>
+      <AmbulanceScene />
+      <Testimonial />
+      <FooterSection />
+    </>
+  );
+}
+
+export default LandingPage;
